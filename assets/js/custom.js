7874f72e5b59f1b3d8d95061c8eb7ebb841a5e62
imeda <imedabeqaia@gmail.com>
2019-10-07 15:03:24 +0400

    nav hover changing
    nav hover changing

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,3 +15,10 @@ $(".filter-input").find("input").blur(function() {
 })
 // $(".slick-active.slick-center").first().siblings().css("width","360px")
 $(".slick-dots").children("li").find("button").text("");
+$(".active-nav-link").siblings("li").mouseover("on", function(){
+    $("li.active-nav-link").attr("id","active-nav-link");
+    $("li.active-nav-link").removeClass("active-nav-link");
+})
+$(".top-header-nav").mouseleave("on", function(){
+    $("#active-nav-link").addClass("active-nav-link");
+})
